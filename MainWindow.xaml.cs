75f212deba3312d16354486d2a9ca09b2fbcc324
IanMcT <ian.mctavish@tldsb.on.ca>
2018-05-10 05:47:58 -0400

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+/*
+ * Demonstration of excursion by 
+ * determining if a number is prime or not
+ */
+
+namespace recusionExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            int checkPrime = 1201;
+            MessageBox.Show(checkPrime.ToString() + " is prime: " + isPrime(checkPrime).ToString());
+        }
+        public bool isPrime(int n)
+        {
+            if (n == 1)
+            {
+                return true;
+            }
+            else if (n < 1)
+            {
+                throw new Exception("Number must be a positive integer");
+            }
+            
+                return isPime(n, n - 1);
+        }
+
+        public bool isPime(int n, int c)
+        {
+            if (c == 1) {
+                return true;
+            }else if (n % c == 0)
+            {
+                return false;
+            }
+            else {
+                return isPime(n, c - 1);
+            }
+        }
+    }
+}
